d356c02fbe05b4a2c73417173aac3fe9639b7839
Fahreza Dhia <fahrezadhia16@gmail.com>
2025-02-13 07:28:52 +0700

    Learning progress

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] test)
+        {
+            string thisString = "ASD";
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"{test[0]}");
+            Console.WriteLine($"{thisString[1]}");
+        }
+    }
+}
---
+++ MVC Freecodecamp/Models/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC_Freecodecamp.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
