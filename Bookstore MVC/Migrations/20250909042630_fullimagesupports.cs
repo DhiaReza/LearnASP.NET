ea78d6fffa960f86551941869654aac5f76fcf94
DhiaReza <fahrezadhia16@gmail.com>
2025-09-11 11:57:58 +0700

    add card view, next add iamge to the card

---
+++ Bookstore MVC/Migrations/20250909042630_fullimagesupports.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookstore_MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class fullimagesupports : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "description",
+                table: "Book",
+                newName: "Description");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageContentType",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageContentType",
+                table: "Book");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Book",
+                newName: "description");
+        }
+    }
+}
--- Bookstore MVC/obj/Debug/net9.0/Bookstore MVC.AssemblyInfo.cs
+++ Bookstore MVC/obj/Debug/net9.0/Bookstore MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ca8ce7c9af27a2adc2ee3c5e618d34257e71da9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8204c8bb00039d5ffaaf62818afe52c57fd39462")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
